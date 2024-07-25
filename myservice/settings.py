7979806589e4bb1fd4cabedef8b21ec5e6c63b94
deshankbisen <63210848+deshankbisen@users.noreply.github.com>
2024-07-26 01:18:15 +0530

    Update settings.py

--- myservice/settings.py
+++ myservice/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-f2m01e(wqy#m$67f0=sjqfd8@y0z(60j@7fvwpfy8v*19qv=bg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    " findmypanditjibackend-d94611cee10f.herokuapp.com",
+    "findmypanditjibackend-d94611cee10f.herokuapp.com",
     "https://findpanditji.netlify.app",
 ]
 
